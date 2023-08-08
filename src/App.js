19f7db3e12d73c9570ad232d58c479317839df5b
SebastianMajcherczyk <seb.majcherczyk@gmail.com>
2023-08-08 17:33:27 +0200

    PA-2 Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Content2 } from './content2';
 
 export const App = () => {
 	return (
 		<div>
 			<p>Something to display</p>
 			<p>Something</p> 
+			<Content2/>
 		</div>
 	);
 };
---
+++ src/content2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Content2 = () => {
+  return (
+    <div>content2</div>
+  )
+}
