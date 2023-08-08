ad007f52ea0c50ac38a7d70ac0213bd0d4bf2de7
SebastianMajcherczyk <seb.majcherczyk@gmail.com>
2023-08-08 17:21:49 +0200

    PA-1: New fetures added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Content } from './content';
 
 export const App = () => {
 	return (
 		<div>
 			<p>Something to display</p>
 			<p>Something</p> 
+			<Content />
 		</div>
 	);
 };
